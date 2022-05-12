5c3df2c214d7cb25c43fb718f13ddcacd2bf00d0
deankp <dperez0689@gmail.com>
2022-05-12 16:18:14 -0500

    updated styling and psudo code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-//document.getElementById('shoe-image').src = `./images/Jordan 1.png`
+document.getElementById('shoe-image').src = `./images/Jordan 1.png`
 
 // initialize start button
 
 // make appear questionaire with image 
 
 // log a score
+
+//create an array with questions 
