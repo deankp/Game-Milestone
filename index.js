1e8c00555ded6701e83c8bb0399329e6b582c6b2
deankp <dperez0689@gmail.com>
2022-05-12 16:50:21 -0500

    finish css

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 document.getElementById('shoe-image').src = `./images/Jordan 1.png`
 
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
 // initialize start button
+function startGame () {
+    startButton.classList.add('hide');
+    // console.log('started')
+}
 
 // make appear questionaire with image 
 
