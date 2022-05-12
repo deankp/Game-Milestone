d7f7b4a6c55ed434f4ffa14a917af41a67f53d54
deankp <dperez0689@gmail.com>
2022-05-12 17:17:59 -0500

    updated html to have question appear

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-document.getElementById('shoe-image').src = `./images/Jordan 1.png`
+// document.getElementById('shoe-image').src = `./images/Jordan 1.png`
 
 const startButton = document.getElementById('start-btn')
+
+const questionCard = document.getElementById('questions')
 
 startButton.addEventListener('click', startGame)
 
 // initialize start button
 function startGame () {
     startButton.classList.add('hide');
+    questionCard.classList.remove('hide')
     // console.log('started')
 }
 
