2d80798b00dd00a49ee9a82b6c40efe85ec16c58
deankp <dperez0689@gmail.com>
2022-05-12 19:23:57 -0500

    testing more functions

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 // document.getElementById('shoe-image').src = `./images/Jordan 1.png`
-
+// set elements
 const startButton = document.getElementById('start-btn')
 
 const questionCard = document.getElementById('preguntas')
 
-const image = document.getElementById('shoe-image')
+const questionElement = document.getElementById('question')
+
+const answerButton = document.getElementById('answer-buttons')
 
 startButton.addEventListener('click', startGame)
 
 // initialize start button
 function startGame () {
     startButton.classList.add('hide');
+    // set shuffled questions in a function to random
+    shuffledQuestions = questions.sort(() => Math.random())
+    currentQuestion = 0
     questionCard.classList.remove('hide');
+    setNextQuestion();
     
     // console.log('started')
 }
 
-function setNextQuestion () {
-    
+function showQuestions(question) {
+    questionElement.innerText = question.question
 }
+
+function setNextQuestion () {
+    showQuestions(shuffledQuestions[currentQuestion])
+}
+
 
 function selectAnswer () {
 
@@ -28,8 +39,17 @@ function selectAnswer () {
 
 // log a score
 
-//create an array with questions 
+//create an array with questions and image 
 
 const questions = [
-
+    {
+        image: document.getElementById('shoe-image').src = `./images/Jordan 1.png`,
+        question: 'Name This Iconic Shoe:',
+        answers: [
+            { text: 'Nike Air Max 1', correct: false },
+            { text: 'Air Jordan 1', correct: true },
+            { text: 'Nike Dunk', correct: false },
+            { text: 'Air Jordan 11', correct: false}
+        ]
+    }
 ]
